8eebcaa5c8198f66c8f445273ebfd79a2ab0d458
Alexandre Roba <Alexandre.roba@arobanet.be>
2013-10-26 19:09:16 +0200

    Finished the first part of the application

--- HotPie/server.js
+++ HotPie/server.js
@@ -5,8 +5,6 @@
 require('coffee-script');
 
 var express = require('express');
-var routes = require('./routes');
-var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
 
@@ -16,6 +14,7 @@ var app = express();
 app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
+app.set('view options',{layout:true});
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
@@ -28,8 +27,8 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);
-app.get('/users', user.list);
+//Routes
+require('./apps/authentication/routes')(app);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
